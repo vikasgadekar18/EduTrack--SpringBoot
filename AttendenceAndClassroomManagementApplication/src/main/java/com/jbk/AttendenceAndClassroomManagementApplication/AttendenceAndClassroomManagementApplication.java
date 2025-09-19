e99c095f96ea8b7533e72bbded015a92c8af7e22
vikasgadekar18 <vikasgadekar10214@gmail.com>
2025-09-19 22:35:03 +0530

    service layer updated

--- AttendenceAndClassroomManagementApplication/src/main/java/com/jbk/AttendenceAndClassroomManagementApplication/AttendenceAndClassroomManagementApplication.java
+++ AttendenceAndClassroomManagementApplication/src/main/java/com/jbk/AttendenceAndClassroomManagementApplication/AttendenceAndClassroomManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AttendenceAndClassroomManagementApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AttendenceAndClassroomManagementApplication.class, args);
 		System.out.println("Application Started");
 	}
